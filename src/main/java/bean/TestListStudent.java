b6addd0839513a6ebcbd036e783974dd4155478e
ooi2572004 <ooi2572004@stu.o-hara.ac.jp>
2026-04-28 11:47:23 +0900

    色々追加したよ

--- src/main/java/bean/TestListStudent.java
+++ src/main/java/bean/TestListStudent.java
@@ -1,35 +1,26 @@
 package bean;
 
-import java.io.Serializable;
+import java.util.Map;
 
-public class TestListStudent extends User implements Serializable {
-	
-	private String subjectName;
-	private String subjectCd;
-	private int num;
-	private int point;
-	public String getSubjectName() {
-		return subjectName;
-	}
-	public void setSubjectName(String subjectName) {
-		this.subjectName = subjectName;
-	}
-	public String getSubjectCd() {
-		return subjectCd;
-	}
-	public void setSubjectCd(String subjectCd) {
-		this.subjectCd = subjectCd;
-	}
-	public int getNum() {
-		return num;
-	}
-	public void setNum(int num) {
-		this.num = num;
-	}
-	public int getPoint() {
-		return point;
-	}
-	public void setPoint(int point) {
-		this.point = point;
+public class TestListStudent {
+
+	private String studentNo;
+	private String studentName;
+	private String classNum;
+	private Map<String, Integer> points; // 科目ID→得点
+
+	public String getStudentNo() { return studentNo; }
+	public void setStudentNo(String studentNo) { this.studentNo = studentNo; }
+	public String getStudentName() { return studentName; }
+	public void setStudentName(String studentName) { this.studentName = studentName; }
+	public String getClassNum() { return classNum; }
+	public void setClassNum(String classNum) { this.classNum = classNum; }
+	public Map<String, Integer> getPoints() { return points; }
+	public void setPoints(Map<String, Integer> points) { this.points = points; }
+
+	/** 指定した科目IDの点数を返す（存在しない場合はnull） */
+	public Integer getPoint(String subjectId) {
+		if (points == null) return null;
+		return points.get(subjectId);
 	}
 }
---
+++ src/main/java/dao/TestListStudentDao.java
@@ -0,0 +1,91 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import bean.School;
+import bean.Subject;
+import bean.TestListStudent;
+
+public class TestListStudentDao extends Dao {
+
+	private List<TestListStudent> postFilter(ResultSet resultSet) throws Exception {
+		Map<String, TestListStudent> map = new LinkedHashMap<>();
+		try {
+			while (resultSet.next()) {
+				String studentNo   = resultSet.getString("student_no");
+				String subjectCd   = resultSet.getString("subject_cd");
+				int    point       = resultSet.getInt("point");
+				String classNum    = resultSet.getString("class_num");
+
+				TestListStudent tts = map.get(studentNo);
+				if (tts == null) {
+					tts = new TestListStudent();
+					tts.setStudentNo(studentNo);
+					tts.setClassNum(classNum);
+					tts.setPoints(new LinkedHashMap<>());
+					// 学生名はstudentテーブルからJOINして取得
+					try {
+						tts.setStudentName(resultSet.getString("student_name"));
+					} catch (Exception e) {
+						tts.setStudentName("");
+					}
+					map.put(studentNo, tts);
+				}
+				tts.getPoints().put(subjectCd, point);
+			}
+		} catch (SQLException | NullPointerException e) {
+			e.printStackTrace();
+		}
+		return new ArrayList<>(map.values());
+	}
+
+	/**
+	 * クラスと科目を条件に学生別成績一覧を取得
+	 * subject が null の場合は全科目対象
+	 */
+	public List<TestListStudent> filter(String classNum, Subject subject,
+			School school, List<Subject> subjects) throws Exception {
+
+		List<TestListStudent> list = new ArrayList<>();
+		Connection connection = getConnection();
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			StringBuilder sql = new StringBuilder(
+				"select t.student_no, s.student_name, t.class_num, t.subject_cd, t.point " +
+				"from test t " +
+				"join student s on t.student_no = s.student_no " +
+				"where t.school_cd = ? and t.class_num = ?"
+			);
+			if (subject != null) {
+				sql.append(" and t.subject_cd = ?");
+			}
+			sql.append(" order by t.student_no, t.subject_cd");
+
+			statement = connection.prepareStatement(sql.toString());
+			statement.setString(1, school.getSchoolCd());
+			statement.setString(2, classNum);
+			if (subject != null) {
+				statement.setString(3, subject.getSubjectCd());
+			}
+
+			resultSet = statement.executeQuery();
+			list = postFilter(resultSet);
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (statement != null) try { statement.close(); } catch (SQLException sqle) { throw sqle; }
+			if (connection != null) try { connection.close(); } catch (SQLException sqle) { throw sqle; }
+		}
+		return list;
+	}
+}
