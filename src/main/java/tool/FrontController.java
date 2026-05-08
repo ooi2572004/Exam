0652a9c835bea8566d602c816ea0ed3edd9cf0a7
strator82 <soft.strator8258@gmail.com>
2026-05-08 11:05:24 +0900

    テストコードの削除

--- src/main/java/tool/FrontController.java
+++ src/main/java/tool/FrontController.java
@@ -1,5 +1,5 @@
 package tool;
- 
+
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
@@ -7,32 +7,28 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
- 
+
 @WebServlet(urlPatterns = { "*.action" })
 public class FrontController extends HttpServlet {
- 
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		try {
-			// パスを取得し、クラス名に変換する（元の完璧なロジック）
+			// パスを取得し、クラス名に変換する
 			String path = req.getServletPath().substring(1);
 			String name = path.replace(".a", "A").replace('/', '.');
 			
-			System.out.println("★FrontControllerが探しているクラス: " + name);
- 
 			Action action = (Action) Class.forName(name).getDeclaredConstructor().newInstance();
 			action.execute(req, res);
- 
+
 		} catch (Exception e) {
-			e.printStackTrace(); // コンソールに本当のエラーを出す
-			// error.jspがないと404になるので、一旦コメントアウトします
-			// req.getRequestDispatcher("/error.jsp").forward(req, res);
+			e.printStackTrace();
+			req.getRequestDispatcher("/error.jsp").forward(req, res);
 		}
 	}
- 
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		doGet(req, res);
 	}
 }
- 
